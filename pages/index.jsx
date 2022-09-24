1bbd92b9310ea091420803a815482ededf20095a
1thdNY <tjgbjg504@gmail.com>
2022-09-24 20:46:08 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "jwZCbBx6gTL8MgoK-g0zoFHqJLpPYwH6"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "jwZCbBx6gTL8MgoK-g0zoFHqJLpPYwH6"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
